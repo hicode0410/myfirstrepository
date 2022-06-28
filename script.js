b75cec2596f65b715b26ad87e0db49bc1ee75512
Muhammad Khilmi <105735550+hicode0410@users.noreply.github.com>
2022-06-28 17:05:45 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Hamburger Menu
+const hamburger = document.querySelector(".hamburger-menu-container");
+const navMenu = document.querySelector("nav ul");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll("li").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
